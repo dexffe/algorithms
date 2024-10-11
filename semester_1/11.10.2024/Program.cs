2a29a8ddc265c7e9c5724019a5a967ab0b45ddb9
dexffe <dexffe@gmail.com>
2024-10-11 14:09:37 +0600

    11.10.2024 (laba_4)

---
+++ semester_1/11.10.2024/Program.cs
@@ -0,0 +1,25 @@
+﻿bool isOver = false;
+int lastNum = 0;
+int endN = 0;
+
+int number = int.Parse(Console.ReadLine());
+if (number <= 0) {
+    isOver = true;
+    Console.WriteLine("Invalid number");
+}
+
+while (!isOver) {
+    if (number < 10) {
+        isOver = true;
+    }
+    lastNum = number % 10;
+    number /= 10;
+    if (lastNum % 2 != 0) {
+        endN = endN * 10 + lastNum;
+    }
+}
+if (endN == 0) {
+    Console.WriteLine("Invalid number");
+} else {
+    Console.WriteLine(endN);
+}
