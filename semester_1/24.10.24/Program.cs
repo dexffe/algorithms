05a901cf9ec4b5c08facf933b98e55f1679aa344
dexffe <dexffe@gmail.com>
2024-10-26 12:12:03 +0600

    24.10.24

---
+++ semester_1/24.10.24/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+int[] CreateArreyList(){
+    int n = int.Parse(Console.ReadLine());
+    int[] list = new int[n];
+    for (int i = 0; i < n; i++) {
+        list[i] = int.Parse(Console.ReadLine());
+    }
+    return list;
+}
+
+
+int[] ReformatList(int[] list){
+    for (int i = 0; i < list.Length; i++) {
+        list[i] = list[i] * list[i];
+    }
+    return list;
+}
+
+
+void OutputList(int[] list){
+    for (int i = 0; i < list.Length; i++) {
+        Console.WriteLine(list[i]);
+    }
+}
+
+OutputList(ReformatList(CreateArreyList()));
+
