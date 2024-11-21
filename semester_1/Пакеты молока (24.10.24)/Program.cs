61b83f8e895e9836580054b0ca2ab0d6ec6ef96e
dexffe <dexffe@gmail.com>
2024-11-21 14:19:04 +0600

    Пакеты молока completed

--- semester_1/Пакеты молока (24.10.24)/Program.cs
+++ semester_1/Пакеты молока (24.10.24)/Program.cs
@@ -1,31 +1,22 @@
 ﻿int n = int.Parse(Console.ReadLine());
 int fabrika = 0;
-int x1, x2, x3, y1, y2, y3;
+float x1, x2, x3, y1, y2, y3;
 float c1, c2;
 float V1, V2, S1, S2;
 float price_milk, min_price_milk = 99999999;
 
 for (int i = 0; i < n; i++)
 {
-    /*var toConvert = Console.ReadLine().Split(' ');
-    int[] numbers1 = new int[6];
-    for (int j = 0; j < 6; j++) {
-        numbers1[j] = int.Parse(toConvert.Take(6).ToArray()[j]);
-    }
-    float[] numbers2 = new float[2];
-    for (int j = 0; j < 6; j++) {
-        numbers2[j] = float.Parse(toConvert.Skip(6).Take(2).ToArray()[j]);
-    }*/
-    var toConvert = Console.ReadLine().Split(' ');
+    string[] toConvert = Console.ReadLine().Split(' ');
 
-    x1 = int.Parse(toConvert[0]);
-    x2 = int.Parse(toConvert[1]);
-    x3 = int.Parse(toConvert[2]);    
-    y1 = int.Parse(toConvert[3]);
-    y2 = int.Parse(toConvert[4]);
-    y3 = int.Parse(toConvert[5]);
-    c1 = Convert.ToSingle(toConvert[6]);
-    c2 = float.Parse(toConvert[7]);
+    x1 = float.Parse(toConvert[0]);
+    x2 = float.Parse(toConvert[1]);
+    x3 = float.Parse(toConvert[2]);    
+    y1 = float.Parse(toConvert[3]);
+    y2 = float.Parse(toConvert[4]);
+    y3 = float.Parse(toConvert[5]);
+    c1 = float.Parse(toConvert[6].Replace('.', ','));
+    c2 = float.Parse(toConvert[7].Replace('.', ','));
 
     V1 = x1 * x2 * x3;
     V2 = y1 * y2 * y3;
@@ -40,4 +31,4 @@ for (int i = 0; i < n; i++)
         fabrika = i + 1;
     }
 }
-Console.WriteLine(fabrika + " " + min_price_milk);
+Console.WriteLine(fabrika + " " + Math.Round(min_price_milk, 2));
