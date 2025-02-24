7a823cb9d85662414dbfcb5bf28c7c253ed4970d
dexffe <dexffe@gmail.com>
2025-02-24 08:56:49 +0600

    testing_stake

---
+++ semester_1/24.02.25/Program.cs
@@ -0,0 +1,30 @@
+﻿string equation = Console.ReadLine();
+Stack<string> stack_brackets = new Stack<string>();
+
+for (int i = 0; i < equation.Length; i++) {
+    if (equation[i] == '(') {
+        stack_brackets.Push("(");
+    } else if (equation[i] == ')') {
+        if (stack_brackets.Count > 0 && stack_brackets.Peek() == "(") {
+            stack_brackets.Pop();
+        }
+    } else if (equation[i] == '{') {
+        stack_brackets.Push("{");
+    } else if (equation[i] == '}') {
+        if (stack_brackets.Count > 0 && stack_brackets.Peek() == "{") {
+            stack_brackets.Pop();
+        }
+    } else if (equation[i] == '[') {
+        stack_brackets.Push("[");
+    } else if (equation[i] == ']') {
+        if (stack_brackets.Count > 0 && stack_brackets.Peek() == "[") {
+            stack_brackets.Pop();
+        }
+    }
+}
+
+if (stack_brackets.Count == 0) {
+    Console.WriteLine("YES");
+} else {
+    Console.WriteLine("NO");
+}
