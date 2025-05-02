1c348b25b3e6bddae85ed2ad5f78a0ae34a4ea7d
dexffe <dexffe@gmail.com>
2025-05-02 21:16:08 +0600

    24.03.25 (Point) in prosess

---
+++ semester_2/24.03.25/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Point {
+    public int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
+class Program {
+    static void Main(string[] args) {
+        //* static field
+        int[] top_r = [2, 15];
+        int[] top_l = [-10, 15];
+        int[] bottom_r = [2, -40];
+        int[] bottom_l = [-10, -40];
+
+        while (true) {
+            
+        }
+
+    }
+}
