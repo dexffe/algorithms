75317268f6ff06d5b7d44f0c6553e3b6b8c807ec
dexffe <dexffe@gmail.com>
2025-05-08 18:14:59 +0600

    fix

--- semester_2/10.04.25/obj/Debug/net9.0/10.04.25.AssemblyInfo.cs
+++ semester_2/10.04.25/obj/Debug/net9.0/10.04.25.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("10.04.25")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a66102b9507c8c7c4159ac4003ac4a290e3b19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e9fd2211c7a962ae2baa87e46a18d89da84c0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("10.04.25")]
 [assembly: System.Reflection.AssemblyTitleAttribute("10.04.25")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
