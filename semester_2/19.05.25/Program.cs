3e129db6ecb5a354395640322f1f1fc12ef69c10
dexffe <dexffe@gmail.com>
2025-05-19 09:57:25 +0600

    19.05.25 completed

---
+++ semester_2/19.05.25/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program {
+    static bool IsPalindrom(int num) {
+        if (num < 0) return false;
+        int original = num;
+        int reversed = 0;
+
+        while (num > 0) {
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+        }
+        return original == reversed;
+    }
+
+    static unsafe void Main() {
+        Console.WriteLine("Введите размер массива:");
+        int size = int.Parse(Console.ReadLine());
+        int* array = stackalloc int[size];
+        Console.WriteLine($"Введите {size} элементов массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Палиндромы в массиве:");
+        for (int i = 0; i < size; i++) {
+            if (IsPalindrom(array[i])) {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
