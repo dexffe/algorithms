d1a1e12375645ae65a3656cc22b8f2b1ed740680
dexffe <dexffe@gmail.com>
2025-05-22 10:30:23 +0600

    22.05.25 completed

---
+++ semester_2/22.05.25/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    unsafe static void Main()
+    {
+        bool isEnd = true;
+
+        int* dict = stackalloc int[256];
+        for (int i = 0; i < 256; i++)
+        {
+            dict[i] = 0;
+        }
+        while (isEnd)
+        {
+            string s = Console.ReadLine();
+            isEnd = s == "" ? false : true;
+            foreach (char c in s)
+            {
+                dict[c]++;
+            }
+        }
+        for (int i = 0; i < 256; i++)
+        {
+            if (dict[i] != 0)
+            {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    Console.WriteLine($"{(char)i}: {dict[i]}");
+            }
+        }
+    }
+}
---
+++ semester_2/Города/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args)
+    {
+        string countRoads = Console.ReadLine().Split(' ')[1];
+        Dictionary<int, int[]> dict = new Dictionary<int, int[]>();
+        unsafe
+        {
+        Dictionary<string, int>* frequency = stackalloc Dictionary<string, int>[256];
+            
+        }
+
+        
+        
+    }
+}
